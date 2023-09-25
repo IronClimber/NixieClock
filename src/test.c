c4fb6a337cd96cf3168210adf23f243cdfc71e23
Stepan Kostusiev <ironclimber@gmail.com>
2023-09-25 19:11:45 +0300

    Get data from RT

--- src/test.c
+++ src/test.c
@@ -1,6 +1,6 @@
 #define F_CPU 8000000UL
 
-//#define TEST_INDICATOR
+#define TEST_INDICATOR
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -26,8 +26,8 @@
 #define AL3_PIN    PB5
 #define AL4_PIN    PB4
 
-#define I2C_SCL_PIN    PA0
-#define I2C_SDA_PIN    PA1
+#define I2C_SCL_PIN    PA1
+#define I2C_SDA_PIN    PA0
 
 //Indicator
 uint8_t indi[4] = { 0x00 };
@@ -41,20 +41,6 @@ uint8_t digits[] = {
  0x1A, 0x11, 0x13, 0x19, 0x12, 0x18, 0x1C, 0x10, 0x14, 0x1B
 };
 
-//System functions
-#define nop() asm volatile ("nop")
-
-void dummyloop(unsigned int timetoloop)
-{ 
-    //PORTD ^= (1 << LED_PIN);
-    _delay_us(5);
-  	/*while (timetoloop>0) 
-	{
-	nop();
-	timetoloop--;
-  	}*/
-}
-
 //Interrupt timer init
 void timer1_init() {
     //Set timer 1 to CTC mode (Clear Timer on Compare
@@ -63,19 +49,12 @@ void timer1_init() {
     // Установка предделителя таймера 1 в 256 (
     TCCR1B |= (1 << CS12);
 
-    // Установка начального значения счетчика таймера 0 (для 1 секунды при F_CPU=8МГц)
-    //TCNT1 = 65536 - 60000; // 256 - (8000000 / 1024 / 2)
-
     //OCR - Output Compare Register
     OCR1A = 110;
 
     // Разрешение прерывания по переполнению таймера 0
     TIMSK = (1 << OCIE1A);
 }
-
-#define USI_DELAY (50)
-#define USISR_8BIT 0xF0
-#define USISR_1BIT 0xFE
 
 //I2C (TWI library)
 void view_state() {
@@ -93,146 +72,151 @@ void view_state() {
 
 }
 
-uint8_t get_counter() {
-    uint8_t counter = 0x0F & USISR;
-    return counter;
+void i2c_delay() {
+    _delay_us(12);
 }
+
+void i2c_delay_half() {
+    _delay_us(6);
+}
+
 void i2c_init() {
-    //USICR = (1 << USIWM1) | (1 << USICS1); //2-wire mode & external positive edge
-    
-    //SDA
+
     DDRA |= (1 << I2C_SDA_PIN); //SDA as output
-      
-    //SCL 
     DDRA |= (1 << I2C_SCL_PIN); //SCL as output
         
-    USICR = (0 << USISIE) | (0 << USIOIE) |
-            (1 << USIWM1) | (0 << USIWM0) |
-            (1 << USICS1) | (0 << USICS0) |
-            (1 << USICLK) | (0 << USITC);
-            
-    USISR = (1 << USISIF) | (1 << USIOIF) |
-            (1 << USIPF)  | (1 << USIDC)  |
-            (0x0 << USICNT0);
-
     PORTA |= (1 << I2C_SDA_PIN); //SDA high
     PORTA |= (1 << I2C_SCL_PIN); //SCL high
 }
 
 void i2c_start() {
     
+
     PORTA |= (1 << I2C_SDA_PIN); // Выставляем исходное состояние SDA=1 на всякий случай
     PORTA |= (1 << I2C_SCL_PIN); // Выставляем исходное состояние SCL=1 на всякий случай
     
-    DDRA  &= ~(1 << I2C_SCL_PIN); // Отключаем SCL от выходного буфера интерфейса, тоесть переводим его в input
     
-    USISR = (1 << USISIF) | (1 << USIOIF) | // Сбрасываем USISR
-            (1 << USIPF)  | (1 << USIDC)  |
-            (0x0 << USICNT0);
-    
+
+    i2c_delay_half();	
     PORTA &= ~(1 << I2C_SDA_PIN); // Переводим SDA=0 пока SCL=1
     
-    dummyloop(USI_DELAY);	
     
-    DDRA  |= (1 << I2C_SCL_PIN); // Подключаем SCL обратно к выходному буферу интерфейса
+
+    i2c_delay_half();
     PORTA &= ~(1 << I2C_SCL_PIN); // Переводим SCL=0
-    PORTA |= (1 << I2C_SDA_PIN); // SDA=1, Освобождаем линию данных для последующего приёма, передачи даных
     
-    dummyloop(USI_DELAY);
     
+    i2c_delay_half();
+        
 }
 
 void i2c_stop() {
     
-    PORTA &= ~(1 << I2C_SCL_PIN);
-    dummyloop(USI_DELAY);
-
-    PORTA &= ~(1 << I2C_SDA_PIN);
-    dummyloop(USI_DELAY);
-
-    PORTA |= (1 << I2C_SDA_PIN);
-    dummyloop(USI_DELAY);
+    i2c_delay();
 
     PORTA |= (1 << I2C_SCL_PIN);
-    dummyloop(USI_DELAY);
+    
+    i2c_delay();
+    PORTA |= (1 << I2C_SDA_PIN);
+    
+    i2c_delay();
 
-    USISR |= (1 << USIPF);
 }
     
-uint8_t i2c_transfer(uint8_t tr) {
+uint8_t i2c_write(uint8_t byte) {
     
-    USISR = tr;
+
+    // Send a byte
+    for (uint8_t i = 0; i < 8; i++) {
+        if (byte & 0x80) {
+            PORTA |= (1 << I2C_SDA_PIN); // Set SDA high (bit is 1)
+        } else {
+            PORTA &= ~(1 << I2C_SDA_PIN); // Set SDA low (bit is 0)
+        }
+	i2c_delay();
+        
+
+        byte <<= 1;  // Shift byte to the left
+        PORTA |= (1 << I2C_SCL_PIN); // Set SCL high to clock in the bit
+        
+        
+
+        i2c_delay();
+        PORTA &= ~(1 << I2C_SCL_PIN); // Set SCL low
+
+        
+        
+    }
+
+    DDRA &= ~(1 << I2C_SDA_PIN);
+    //PORTA &= ~(1 << I2C_SDA_PIN); // Set SDA as input ??? DDRA?
+    i2c_delay();
+    PORTA |= (1 << I2C_SCL_PIN);  // Set SCL high
+    i2c_delay_half();
     
-    tr = (0 << USISIE) | (0 << USIOIE) |  //Заготовка для USICR
-         (1 << USIWM1) | (0 << USIWM0) |
-         (1 << USICS1) | (0 << USICS0) |
-         (1 << USICLK) | (1 << USITC);    //USITC=1 чтобы сдвинуть регистр и перевернуть SCL
     
     
 
-    do {
-        dummyloop(USI_DELAY);
-        USICR=tr;                        // Поидее это должно перевернуть SCL 
-        indi[1] = digits[0x0F & USISR + 10];
-        while(bit_is_clear(PINA,I2C_SCL_PIN)) { } //Wait for setting SCL
-        view_state();
-        dummyloop(USI_DELAY);
-        USICR=tr;                        // Это должно перевернуть SCL
-        
-    } while (!(USISR&(1<<USIOIF))); // Пока не переполнился счётчик
+     // Generate an ACK
+    uint8_t ack = 1;
+    if (PINA & (1 << I2C_SDA_PIN)) {
+        ack = 1; // NACK received
+    } else {
+        ack = 0; // ACK received
+    }
+
     
-    dummyloop(USI_DELAY);
+    i2c_delay_half();
+
     
-    tr = USIDR;
-    USIDR=0xff;
+    PORTA &= ~(1 << I2C_SCL_PIN); // Set SCL low
+    //PORTA |= (1 << I2C_SDA_PIN);  // Set SDA as output ??? DDRA?
     DDRA |= (1 << I2C_SDA_PIN);
+    PORTA &= ~(1 << I2C_SDA_PIN);
     
-    return tr;    
-    
-}
-    
-uint8_t i2c_write(uint8_t *data, uint8_t data_size) {
-    
-    //if (PINA & (1 << I2C_SCL_PIN)) {return 1;}
-    if (bit_is_set(PINA, I2C_SCL_PIN)) { return 1;}    
 
-    do {
-        USIDR=(*(data++));   // Предварительно загружаем регистр данными
-        
-        i2c_transfer(USISR_8BIT);  // Отправляем данные
-        
-        DDRA &= ~(1 << I2C_SDA_PIN);         //Switch to input
-        if (i2c_transfer(USISR_1BIT)&0x01) {return 2;}  // Получаем подтверждение о полученнии
-        data_size--;
-    } while (data_size>0);
-
-    return 0;
-    
-    //USIDR = data;
-    //USISR = (1 << USIOIF); // Сброс флага прерывания USI
-    //while (!(USISR & (1 << USIOIF))) {
-    //    USICR |= (1 << USITC);
-    //}
+    return ack;
 }
 
-uint8_t i2c_read(uint8_t *data, uint8_t data_size) {
+uint8_t i2c_read(uint8_t ack) {
     
-    if (PINA & (1 << I2C_SCL_PIN)) {return 1;}
-    do {
-        DDRA |= (1 << I2C_SDA_PIN);
-        *(data++) = i2c_transfer(USISR_8BIT);
+    uint8_t byte = 0;
+
+    //PORTA &= ~(1 << I2C_SDA_PIN);
+    DDRA &= ~(1 << I2C_SDA_PIN);
+    
+    for (uint8_t i = 0; i < 8; i++) {
+        i2c_delay();
+        PORTA |= (1 << I2C_SCL_PIN);
+        i2c_delay_half();
+        byte <<= 1;
+        if (PINA & (1 << I2C_SDA_PIN)) {
+            byte |= 0x01;
+            //indi[3] = digits[1+30];
+        }
+        else {
+            //indi[3] = digits[0+30];
+        }
+        i2c_delay_half();
+        PORTA &= ~(1 << I2C_SCL_PIN);
         
-        if (data_size==1) { USIDR = 0xFF; }
-        else { USIDR = 0x00; }
-        i2c_transfer(USISR_1BIT);
-        data_size--;
-    } while (data_size>0);
-    return 0;
-    //USISR = (1 << USIOIF); // Сброс флага прерывания USI
-    //while (!(USISR & (1 << USIOIF))) {
-    //    USICR |= (1 << USITC);
-    //}
-    //return USIDR;
+    }
+
+    DDRA |= (1 << I2C_SDA_PIN);
+	
+    if (ack) {
+        PORTA |= (1 << I2C_SDA_PIN);
+    }
+    else {
+        PORTA &= ~(1 << I2C_SDA_PIN);
+    }
+    i2c_delay();
+    PORTA |= (1 << I2C_SCL_PIN);
+    i2c_delay();
+    PORTA &= ~(1 << I2C_SCL_PIN);
+
+    return byte;
+
 }
 
 // RTC
@@ -267,33 +251,12 @@ struct rtc_time realtime;
 struct rtc_date realdate;
 
 uint8_t rtc3231a_init(void) {
-    
-    
-    uint8_t write_buf[4];
-    write_buf[0] = RTC_WADDR;
-    write_buf[1] = RTC_CONTROL_REGISTER;
-    write_buf[2] = 0x20;
-    write_buf[3] = 0x08;
-    
+       
     i2c_start();
-    
-    dummyloop(USI_DELAY);
-
-    switch (i2c_write(write_buf,4)) {
-
-        case 1: {		// Error SCL
-            return 1;
-        }
-        case 2: {		// NASC
-            return 2;
-        };
-        default : {		// Ok
-            break;
-        };
-    };
-    
-    dummyloop(USI_DELAY);
-
+    if (i2c_write(RTC_WADDR)) {return 1; }
+    if (i2c_write(RTC_CONTROL_REGISTER)) { return 2; }
+    if (i2c_write(0x20)) { return 3; }
+    if (i2c_write(0x08)) { return 4; }
     i2c_stop();
 
     return 0;
@@ -303,76 +266,19 @@ uint8_t rtc3231a_init(void) {
 }
 
 uint8_t rtc3231a_read_time(struct rtc_time *time) {
-    uint8_t write_buf[3];
-    write_buf[0] = RTC_WADDR;
-    write_buf[1] = 0x00;
-    
-    //Check if SDA == 0 and SCL == 0, then continue
-    //if (!(PINA & (1 << I2C_SCL_PIN)) || !(PINA & (1 << I2C_SDA_PIN))) {return 1;}
-    view_state();
-    if ((bit_is_clear(PINA,I2C_SCL_PIN))||(bit_is_clear(PINA, I2C_SDA_PIN))) {return 1;}
-    view_state();
+   
     i2c_start();
-    view_state();
-    dummyloop(USI_DELAY);
-    view_state();
-    switch (i2c_write(write_buf,2)) {
-    
-	case 1: {
-		return (2); // Error SCL
-	}
-	case 2: {
-		return (3); // NASC
-	}
-	default: {
-		break; // Ok
-	}
-    }	
-    write_buf[0] = RTC_RADDR;
-    dummyloop(USI_DELAY);
-    view_state();
-    i2c_start();
-    view_state();
-    switch (i2c_write(write_buf,1)) {
-
-	case 1: {
-		return(4); //Error SCL
-	}
-	case 2: {
-		return(5); // NASC
-	}
-	default: {
-		break; // Ok
-	}
-    }
-    dummyloop(USI_DELAY);
-    view_state();
-    switch (i2c_read(write_buf, 3)) {
-
-	case 1: {
-		return (6);
-	}
-	default: {
-		break;
-	}
-    }
-    dummyloop(USI_DELAY);
-    view_state();
+    if (i2c_write(RTC_WADDR)) { return 1; }
+    if (i2c_write(0x00)) { return 2; }
     i2c_stop();
-view_state();
-    
-    time->sec = bcd(write_buf[0]);
-    time->min = bcd(write_buf[1]);
-    time->hour = bcd(write_buf[2]);
-    
-    return(0);
+    i2c_start();
+    if (i2c_write(RTC_RADDR)) { return 3; }
+    time->sec = bcd(i2c_read(0));
+    time->min = bcd(i2c_read(0));
+    time->hour = bcd(i2c_read(1));
+    i2c_stop();
 
-    /*i2c_write(RTC_WADDR);
-    i2c_write(0x00);
-    i2c_write(RTC_RADDR);
-    time->sec = bcd(i2c_read());
-    time->min = bcd(i2c_read());
-    time->hour = bcd(i2c_read());*/
+    return 0;
 }
 
 void rtc3231a_read_date(struct rtc_date *date) {
@@ -413,10 +319,13 @@ int main(void) {
 
     timer1_init();
     sei();
-
+    _delay_ms(3000);
     i2c_init();
     rtc3231a_init();
-    
+    //indi[3] = digits[rtc3231a_init() + 30];
+    _delay_ms(50);
+    //rtc3231a_read_time(&realtime);
+
     while (1) {
     
 #ifdef TEST_INDICATOR    
@@ -426,10 +335,12 @@ int main(void) {
     indi[3] = digits[counter+30];
     counter++;
     if (counter>9) { counter = 0; }
-#endif
-    //indi[3] = digits[rtc3231a_read_time(&realtime)+30];
-    indi[3] = digits[5+30];
-    
+
+#else
+    //indi[1] = digits[rtc3231a_read_time(&realtime)+10];
+    //indi[3] = digits[5+30];
+       
+       rtc3231a_read_time(&realtime);
        //rtc3231a_read_date(&realdate);
         
     	/*if (!(PIND & 0x02)) { //Не нажата ли кнопка SET
@@ -438,18 +349,19 @@ int main(void) {
     	if (!(PIND & 0x01)) { //Не нажата ли кнопка MODE
     		PORTD &= 0xFB;  //Выключить LED
     	} */
-       
+    //rtc3231a_read_time(&realtime);
       //PORTD^= (1 << LED_PIN);
       PORTD^= (1 << NEON_PIN);
       //PORTD ^= (1 << BUZZER_PIN);
         // Пауза в 1 секунду
-      /*
-      indi[0] = (uint8_t)realtime.min/10;
-      indi[1] = (uint8_t)realtime.min%10;
-      indi[2] = (uint8_t)realtime.sec/10;
-      indi[3] = (uint8_t)realtime.sec%10;
-      */
-      _delay_ms(1000);
+      
+      indi[0] = digits[(uint8_t)realtime.min/10];
+      indi[1] = digits[(uint8_t)realtime.min%10+10];
+      indi[2] = digits[(uint8_t)realtime.sec/10+20];
+      indi[3] = digits[(uint8_t)realtime.sec%10+30];
+      
+#endif
+      _delay_ms(500);
     }
 
     return 0;
